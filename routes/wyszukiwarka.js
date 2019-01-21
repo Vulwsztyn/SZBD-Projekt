dc5288c38e92f65d6cf324e4c2a5c9d6035057e8
Luker <luker.plus@gmail.com>
2019-01-21 21:36:50 +0100

    almost

--- routes/wyszukiwarka.js
+++ routes/wyszukiwarka.js
@@ -2,27 +2,31 @@ var selectFun = require ('../connections/select');
 module.exports = function(app) {
     app.get('/wyszukiwarka', async function  (req, res) {
         if(req.query.wyszukaj==='1'){
-            const sql = 'select * from '+req.query.which+' where '+req.query.param+'=:i';
-            const binds = {
-                i:req.query.input,
-            };
             const wynik = await selectFun.select(sql,binds,{});
-            if(req.query.which==='osoby')
+            if(req.query.which==='osoby'){
+                //sql
                 res.render('wyszukiwarka',{
                     osoby:wynik
                 });
-            if(req.query.which==='wydzialy')
+            }
+            if(req.query.which==='wydzialy'){
+                //sql
                 res.render('wyszukiwarka',{
                     wydzialy:wynik
                 });
-            if(req.query.which==='kierunki')
+            }
+            if(req.query.which==='kierunki'){
+                //sql
                 res.render('wyszukiwarka',{
                     kierunki:wynik
                 });
-            if(req.query.which==='przedmioty')
+            }
+            if(req.query.which==='przedmioty'){
+                //sql
                 res.render('wyszukiwarka',{
                     przedmioty:wynik
                 });
+            }
         }else res.render('wyszukiwarka',{});
     });
 }
